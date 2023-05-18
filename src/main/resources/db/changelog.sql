c067569c2100bfdc616a90af0100b76e3890f73b
Tsebal <atsebal@gmail.com>
2023-05-18 16:34:00 +0300

    Task 02: Refactored: VK and Yandex are completely removed.

--- src/main/resources/db/changelog.sql
+++ src/main/resources/db/changelog.sql
@@ -266,3 +266,7 @@ INSERT INTO user_belong (id, object_id, object_type, user_id, user_type_code, st
 INSERT INTO user_belong (id, object_id, object_type, user_id, user_type_code, startpoint, endpoint) VALUES (4, 3, 2, 2, 'admin', null, null);
 INSERT INTO user_belong (id, object_id, object_type, user_id, user_type_code, startpoint, endpoint) VALUES (5, 4, 2, 2, 'admin', null, null);
 INSERT INTO user_belong (id, object_id, object_type, user_id, user_type_code, startpoint, endpoint) VALUES (6, 5, 2, 2, 'admin', null, null);
+
+--changeset tsebal:remove_vk_social
+DELETE FROM REFERENCE WHERE CODE = 'vk';
+DELETE FROM CONTACT WHERE CODE = 'vk';
